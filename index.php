28bd8606cee32ab298358b193782899cdba5d7ea
tamaraputri8060 <tamaraputri8060@gmail.com>
2025-11-13 13:55:03 +0700

    proses login

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
+<?php
+session_start();
 
+if( isset($_SESSION['username']) ) {
+    header("Location: dashboard.php");
+    exit;
+}
 
+if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    if( $username == 'Tamara' && $password == 'Putri' ) {
+        $_SESSION['username'] = $username;
+        header("Location: dashboard.php");
+        exit;
+    }
+    else {
+        $error = "usernamenya & passwordnya salah";
+    }
+
+}
+
+?>
 <!DOCTYPE html>
 <html lang="id">
 <head>
